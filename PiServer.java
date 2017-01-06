f6fdb07f613498b308fdd681248672a989180738
sarveshharale10 <sarveshharale10@gmail.com>
2017-01-06 08:34:21 +0530

    Add files via upload

---
+++ PiServer.java
@@ -0,0 +1,52 @@
+import java.io.*;
+import java.util.*;
+
+class PiServerThread extends Thread{
+
+
+
+	
+	PiServerThread(){
+
+
+
+	}
+
+	int[] extractCommandBits(String commandString){
+
+
+	}
+
+	int getOnOffState(int[] commandFields){
+
+
+	}
+
+	GpioPinDigitalOutput getDevicePin(int[] commandFields){
+
+
+	}
+
+	int getScheduleTime(int[] commandFields){
+
+
+	}
+
+	BitSet getDimmerLevel(int[] commandFields){
+
+
+	} 
+
+	public void run(){
+
+
+	}
+
+}
+
+class PiServer{
+
+	public static void main(String arg[]){
+
+	}
+}
